8c2e09217b4aae82515d68d855fd2a873ff5468e
Nathan Wooten <a8gid24akL@gmail.com>
2023-02-11 11:49:35 -0500

    blank index

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
 
-$dir = dirname( __FILE__ );
-$file = basename( __FILE__ );
-
-foreach ( scandir( $dir ) as $item ) {
-  if ( '.' === $item || '..' === $item || $file === $item ) {
-    continue; 
-  }
-  require_once $dir . DIRECTORY_SEPARTOR . $item;
-}
