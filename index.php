fa3b4e42bb3ace1b6f5cad58d19ff7e09f4b5291
Nathan Wooten <a8gid24akL@gmail.com>
2023-02-11 14:11:31 -0500

    removed example, not the place

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
 require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'require.php';
-
-$view = new nathanwooten\View\View;
-$view->set( '<h1>Hello World</h1><p>This is a hello world document.</p>' );
-$template = $view->getResponse();
-print $template;
